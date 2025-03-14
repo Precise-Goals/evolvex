22fb289cfb72eff6ad2ce08564a36cdcf5b1596b
Precise Goals <elitesarthakamr@gmail.com>
2025-03-15 02:03:53 +0530

    hoja bhai

--- client/src/components/Code.jsx
+++ client/src/components/Code.jsx
@@ -1,47 +1,7 @@
-import React, { useState } from "react";
+import React from 'react'
 
 export const Code = () => {
-  const [input, setInput] = useState("");
-  const [output, setOutput] = useState([]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ input }),
-    });
-    const data = await response.json();
-    setOutput((prevOutput) => [
-      ...prevOutput,
-      { user: input, bot: data.response },
-    ]);
-    setInput("");
-  };
-
   return (
-    <div>
-      {/* <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Type your message"
-        />
-        <button type="submit">Send</button>
-      </form>
-      <div>
-        {output.map((item, index) => (
-          <div key={index}>
-            <strong>User:</strong> {item.user}
-            <br />
-            <strong>Bot:</strong> {item.bot}
-            <hr />
-          </div>
-        ))}
-      </div> */}
-    </div>
-  );
-};
+    <div>Code</div>
+  )
+}
